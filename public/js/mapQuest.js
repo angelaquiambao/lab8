356e4995bea005bf21807c86a970d6b937910953
Angela Quiambao <aquiamba@ucsd.edu>
2020-02-27 10:32:36 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
-	// add your code here
+	
+	L.mapquest.key = 'RAdwArgGHPdz8GraYH7ghZTDkQGCuaGo';
+	
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
+
 }
